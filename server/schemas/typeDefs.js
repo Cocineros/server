3536e659f7ee1a4e04680f6351d1b60720f04966
Ayse Jones <aysej305@gmail.com>
2022-03-10 21:39:23 -0600

    typedev file done

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+const typeDefs = gql`
+  type Profile {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedRecipes: [recipeSchema]!
+  }
+
+  type Recipe {
+    _id: ID
+    name: String
+    description: String
+    ingredients: String
+    instructions: String
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    me: Profile
+  }
+
+  type Mutation {
+    addProfile(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addRecipe(name: String!,description: String!, ingredients: String!, instructions: String!): Recipe
+    removeRecipe(recipeId: ID!): Recipe
+  }
+`;
+
+module.exports = typeDefs;
