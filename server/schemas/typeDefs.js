47b984ade2b9e13d163f695dcf9d52289f65f9b8
Brian Garland <briangarlandk@gmail.com>
2022-03-11 20:04:07 -0600

    Fixed mutations for recipes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
-    savedRecipes: [recipeSchema]!
+    savedRecipes: [Recipe]!
   }
 
   type Recipe {
@@ -29,7 +29,7 @@ const typeDefs = gql`
     addProfile(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     addRecipe(name: String!,description: String!, ingredients: String!, instructions: String!): Recipe
-    removeRecipe(recipeId: ID!): Recipe
+    removeRecipe(_id: String!): Recipe
   }
 `;
 
